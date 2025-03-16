44776ec3da0a73576eabedebeb224d837343cb28
millc12 <millc12@my.erau.edu>
2025-03-16 15:35:23 -0700

    Update board.cpp
    Functional. No error handling

--- board.cpp
+++ board.cpp
@@ -1,31 +1,45 @@
 #include "Board.h"
 
 Board::Board() {
-	int temp[9][9] = {
-		{1, 2, 3, 4, 5, 6, 7, 8, 9},
-		{9, 1, 2, 3, 4, 5, 6, 7, 8},
-		{2, 3, 4, 5, 6, 7, 8, 9, 1},
-		{1, 2, 3, 4, 5, 6, 7, 8, 9},
-		{9, 1, 2, 3, 4, 5, 6, 7, 8},
-		{2, 3, 4, 5, 6, 7, 8, 9, 1},
-		{1, 2, 3, 4, 5, 6, 7, 8, 9},
-		{9, 1, 2, 3, 4, 5, 6, 7, 8},
-		{2, 3, 4, 5, 6, 7, 8, 9, 1},
-	};
-	bool temp2[9][9] = {
-		{0, 1, 0, 1, 0, 1, 0, 1, 0},
-		{1, 0, 1, 0, 1, 0, 1, 0, 1},
-		{0, 1, 0, 1, 0, 1, 0, 1, 0},
-		{1, 0, 1, 0, 1, 0, 1, 0, 1},
-		{0, 1, 0, 1, 0, 1, 0, 1, 0},
-		{1, 0, 1, 0, 1, 0, 1, 0, 1},
-		{0, 1, 0, 1, 0, 1, 0, 1, 0},
-		{1, 0, 1, 0, 1, 0, 1, 0, 1},
-		{0, 1, 0, 1, 0, 1, 0, 1, 0},
-	};
+	int sol[9][9];
+	int cov[9][9];
+	string line;
+
+	ifstream fin("boards.txt");
+
+	getline(fin, line);
 	for (int i = 0; i < 9; i++) {
 		for (int j = 0; j < 9; j++) {
-			board[i][j] = Block(i, j, temp[i][j], temp2[i][j]);
+			cov[i][j] = line[i * 9 + j] - '0';
+		}
+	}
+
+	getline(fin, line);
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 9; j++) {
+			sol[i][j] = line[i * 9 + j] - '0';
+		}
+	}
+	fin.close();
+
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 9; j++) {
+			board[i][j] = Block(i, j, sol[i][j], cov[i][j]);
 		}
 	}
 }
+
+Block& Board::locate(int x, int y) {
+	return board[x-1][y-1];
+}
+
+int Board::win() {
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 9; j++) {
+			if (board[i][j].correct != 1) return 0;
+		}
+	}
+	cout << "You win!!!!" << endl;
+	return 1;
+}
+
