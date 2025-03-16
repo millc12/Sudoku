c0158d8a84d50472d4dd9d89a2275aa49ba3fad9
millc12 <millc12@my.erau.edu>
2025-03-16 15:36:22 -0700

    Update main.cpp
    functional, no error handling

--- main.cpp
+++ main.cpp
@@ -4,5 +4,22 @@
 int main() {
 	Board b;
 	b.printBoard();
+	int row;
+	int col;
+	int guess;
+
+	do {
+	cout << "Enter a row: ";
+	cin >> row;
+	cout << "Enter a column: ";
+	cin >> col;
+	cout << "Enter a guess: ";
+	cin >> guess;
+	b.locate(row, col).guess(guess);
+	b.locate(row, col).check();
+	b.printBoard();
+	if (b.win()) break;
+	} while (true);
+
 	return 0;
 }
