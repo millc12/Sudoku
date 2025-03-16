e1225fb067ec07acfd5184113ad7b3ea077e913a
millc12 <millc12@my.erau.edu>
2025-03-16 15:35:56 -0700

    Update dispBoard.cpp
    Functional, no error handling

--- dispBoard.cpp
+++ dispBoard.cpp
@@ -4,16 +4,25 @@
 
 
 void Board::printBoard() {
-	cout << "Print board here" << endl;
-
+	string cyan = "\033[36m";
+	string yellow = "\033[33m";
+	string red = "\033[31m";
+	string reset = "\033[0m";
+	system("CLS");
+	cout << red << "   1 2 3   4 5 6   7 8 9" << reset << endl;
 	for (int i = 0; i < 9; i++) {
+		cout << red << i + 1 << "  " << reset;
 		for (int j	 = 0; j < 9; j++) {
-			if (!board[i][j].covered) cout << "  ";
-			else cout << board[i][j].value << " ";
+			if (board[i][j].given)cout << board[i][j].value << " ";
+			else if (board[i][j].userValue == 0) cout << ". ";
+			else cout << cyan << board[i][j].userValue << " " << reset;
+
+			if (j == 2 || j == 5)cout << yellow << "| " << reset;
 		}
 		cout << endl;
+		if (i == 2 || i == 5)cout << yellow << "   ------+-------+------" << reset << endl;
+		
 	}
 };
-
 
 
